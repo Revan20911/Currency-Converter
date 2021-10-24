587073296aec878ad1ea8d76d5684288235add46
Christopher Merrill <47538097+Revan20911@users.noreply.github.com>
2021-10-24 14:01:22 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.Convert = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(231, 142);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(521, 20);
+            this.maskedTextBox1.TabIndex = 0;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(231, 221);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(521, 20);
+            this.maskedTextBox2.TabIndex = 1;
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(59, 282);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(144, 23);
+            this.Convert.TabIndex = 2;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(231, 282);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(144, 23);
+            this.Clear.TabIndex = 3;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "USD",
+            "JPY",
+            "GBP",
+            "EUR",
+            "CHY",
+            "CND",
+            "AUD"});
+            this.comboBox1.Location = new System.Drawing.Point(59, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(144, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "USD",
+            "JPY",
+            "GBP",
+            "EUR",
+            "CHY",
+            "CND",
+            "AUD"});
+            this.comboBox2.Location = new System.Drawing.Point(59, 221);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(144, 21);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    class Conversions
+    {
+        private double USD = 1.00;
+        private double JPY = 0.01;
+        private double CND = 0.75;
+        private double GBP = 1.25;
+        private double currency;
+
+        
+
+        public double Currency { get { return currency; } set { currency = value; }  }
+
+
+        public double GetUSD()
+        {
+            return USD;
+        }
+        public void SetUSD(double USD)
+        {
+            this.USD = USD;
+        }
+
+        public double GetJPY()
+        {
+            return JPY;
+        }
+        public void SetJPY(double JPY)
+        {
+            this.JPY = JPY;
+        }
+
+        public double GetCND()
+        {
+            return CND;
+        }
+        public void SetCND(double CND)
+        {
+            this.CND = CND;
+        }
+
+        public double GetGBP()
+        {
+            return GBP;
+        }
+
+        public void SetGBP(double GBP)
+        {
+            this.GBP = GBP;
+        }
+
+
+    
+
+    }
+
+
+    public partial class Form1 : Form
+    {
+
+        private double Converted;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double getCurrency()
+
+        {
+           double c = new Conversions().Currency;
+
+           double.TryParse(maskedTextBox1.Text, out c);
+           return c;
+        
+        }
+
+        
+
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            var con = new Conversions();
+            
+
+            
+
+            switch (comboBox1.Text)
+            {
+                case "USD":
+                    Converted = getCurrency() * con.GetUSD();
+                    maskedTextBox2.Text = Converted.ToString();
+                    break;
+                case "CND":
+                    Converted = getCurrency() * con.GetCND();
+                    maskedTextBox2.Text = Converted.ToString();
+                    break;
+                case "JPY":
+                    Converted = getCurrency() * con.GetJPY();
+                    maskedTextBox2.Text = Converted.ToString();
+                    break;
+                case "GBP":
+                    Converted = getCurrency() * con.GetGBP();
+                    maskedTextBox2.Text = Converted.ToString();
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            maskedTextBox1.Clear();
+            maskedTextBox2.Clear();
+
+        }
+    }
+}
